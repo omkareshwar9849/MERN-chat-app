bd16baa3443ef4d8076f7bf31c0a209ee2d1feb4
Omkareshwar <omkar.varada9849@gmail.com>
2024-06-17 21:18:37 +0530

    style(frontend): responsive home page

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -14,7 +14,7 @@ const MessageContainer = () => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col'>
+		<div className='flex flex-col w-full md:w-auto md:min-w-[450px]'>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
@@ -33,11 +33,12 @@ const MessageContainer = () => {
 };
 export default MessageContainer;
 
+
 const NoChatSelected = () => {
 	const { authUser } = useAuthContext();
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+			<div className='px-4 text-center text-sm sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
 				<p>Welcome 👋 {authUser.fullName} ❄</p>
 				<p>Select a chat to start messaging</p>
 				<TiMessages className='text-3xl md:text-6xl text-center' />
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
 	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
+		<div className='border-r border-slate-500 p-4 flex flex-col w-full md:w-auto'>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
