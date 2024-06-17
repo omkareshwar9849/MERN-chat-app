e9d678a16ee6e423121af4dc12c6a4647a32e4c6
Omkareshwar <omkar.varada9849@gmail.com>
2024-06-17 20:34:09 +0530

    fix(frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast';
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import SignUp from './pages/signup/SignUp';
+import SignUp from './pages/signup/SignUp.jsx';
 import { useAuthContext } from './context/AuthContext';
 
 function App() {
