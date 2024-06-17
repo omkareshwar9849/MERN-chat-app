f5de1b46cf1fbd70a46ee7c67c70c6192638b06a
Omkareshwar <omkar.varada9849@gmail.com>
2024-06-17 19:52:10 +0530

    feat(chat-app): integrate socket.io

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,27 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connectToMongoDb.js";
 
-dotenv.config();
-const app = express();
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.use(express.json()); // for parsing application/json
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
-app.use("/api/user",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
 
-app.get("/",(req,res)=>{
-    // root route http://localhost:5000
-    res.send("Hello world!");
-})
+// app.get("/", (req, res) => {
+// root route http://localhost:5000/
+// 	res.send("Hello World!!");
+// });
 
-app.listen(PORT,()=>{
-    connectToMongoDB();
-    console.log(`server running on port ${PORT}`);
-})
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the change here
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import { AuthContextProvider } from './context/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<SocketContextProvider>
+					<App />
+				</SocketContextProvider>
+			</AuthContextProvider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
